fe00f85048b61732a1bf398b1081e20aaf1abc10
Rexford Essilfie <rexfordessilfie09@gmail.com>
2022-01-14 18:50:12 -0500

    feat: add pitch_from_frequency

--- frequencies.py
+++ frequencies.py
@@ -1,28 +1,138 @@
+import copy
 from intervals import EqualTemperament as et
-import scales
+from main import Accidental as al, Pitch, PitchClass as pc
 import math
 
-tuning = {"A440": 440}
 
-# steps: get the frequency, divide by 440 and then determine what interval it is by comparing result to possible interval values
-# after determining the interval, we can figure out the note is
+KEYBOARD_PITCHES = [
+    # Pitch<A,natural,4>
+    Pitch(pitch_class=pc.A,
+          accidental=al.NATURAL,
+          register=4,
+          frequency=440),
 
-A4 = 440
-C5 = A4 * et.MINOR_THIRD.value
+    # Pitch<A,sharp,4>
+    Pitch(pitch_class=pc.A,
+          accidental=al.SHARP,
+          enharmonic_pitch_class=pc.B,
+          enharmonic_accidental=al.FLAT,
+          register=4,),
 
-sample_pitch = 880
-print(f"Candidate {sample_pitch}")
+    # Pitch<B,natural,4>
+    Pitch(pitch_class=pc.B,
+          accidental=al.NATURAL,
+          register=4),
 
-c_chromatic_scale = scales.ScaleFactory.get_scale(C5, "chromatic")
-print(f"C chromatic scale {c_chromatic_scale}")
+    # Pitch<C,natural,5>
+    Pitch(pitch_class=pc.C,
+          accidental=al.NATURAL,
+          register=5),
+
+    # Pitch<C,sharp,5>
+    Pitch(pitch_class=pc.C,
+          accidental=al.SHARP,
+          enharmonic_pitch_class=pc.D,
+          enharmonic_accidental=al.FLAT,
+          register=5,),
+
+    # Pitch<D,natural,5>
+    Pitch(pitch_class=pc.D,
+          accidental=al.NATURAL,
+          register=5),
+
+    # Pitch<D,sharp,5>
+    Pitch(pitch_class=pc.D,
+          accidental=al.SHARP,
+          enharmonic_pitch_class=pc.E,
+          enharmonic_accidental=al.FLAT,
+          register=5,),
+
+    # Pitch<E,natural,5>
+    Pitch(pitch_class=pc.E,
+          accidental=al.NATURAL,
+          register=5),
+
+    # Pitch<F,natural,5>
+    Pitch(pitch_class=pc.F,
+          accidental=al.NATURAL,
+          register=5),
+
+    # Pitch<F,sharp,5>
+    Pitch(pitch_class=pc.F,
+          accidental=al.SHARP,
+          enharmonic_pitch_class=pc.G,
+          enharmonic_accidental=al.FLAT,
+          register=5,
+          ),
+
+    # Pitch<G,natural,5>
+    Pitch(pitch_class=pc.G,
+          accidental=al.NATURAL,
+          register=5),
+
+    # Pitch<G,sharp,5>
+    Pitch(pitch_class=pc.G,
+          accidental=al.SHARP,
+          enharmonic_accidental=pc.A,
+          enharmonic_pitch_class=al.FLAT,
+          register=5,),
+]
 
 
-quotient = sample_pitch/A4
+def interval_between(a: float, b: float):
+    return b / a
 
-# Need to figure out how to get a pitch name from frequency (western music system)
-for freq in c_chromatic_scale:
-    division_result = freq/sample_pitch
-    floored_result = math.floor(division_result)
 
-    if division_result == floored_result:
-        print("found")
+def is_above_octave_range(a: float, b: float):
+    return interval_between(a, b) >= et.OCTAVE.value
+
+
+def is_below_octave_range(a: float, b: float):
+    return interval_between(a, b) < et.UNISON.value
+
+
+def pitch_from_frequency(frequency: float):
+    reference_pitch = next(filter(lambda p: p.pitch_class == pc.A and p.accidental == al.NATURAL,
+                                  KEYBOARD_PITCHES), None)
+
+    normalized_interval = frequency / reference_pitch.frequency
+    normalization_scale = 0
+
+    # normalize frequency to same octave
+    while True:
+        below_octave_range = is_below_octave_range(
+            reference_pitch.frequency, reference_pitch.frequency * normalized_interval)
+
+        above_octave_range = is_above_octave_range(
+            reference_pitch.frequency, reference_pitch.frequency * normalized_interval)
+
+        if below_octave_range:
+            normalized_interval *= float(2)
+            normalization_scale -= 1
+        elif above_octave_range:
+            normalized_interval /= float(2)
+            normalization_scale += 1
+        else:
+            break
+
+    num_semitones_from_reference = round(
+        math.log(normalized_interval, et.SEMITONE.value)
+    )
+
+    reference_pitch_idx = next(i for i, v in enumerate(
+        KEYBOARD_PITCHES) if KEYBOARD_PITCHES[i].pitch_class == reference_pitch.pitch_class)
+
+    final_pitch_idx = (reference_pitch_idx +
+                       num_semitones_from_reference) % len(KEYBOARD_PITCHES)
+
+    found_pitch = KEYBOARD_PITCHES[final_pitch_idx]
+    final_pitch = copy.deepcopy(found_pitch)
+
+    final_pitch.register = final_pitch.register + normalization_scale
+    final_pitch.frequency = frequency
+
+    return final_pitch
+
+
+if __name__ == '__main__':
+    print(pitch_from_frequency(440))
