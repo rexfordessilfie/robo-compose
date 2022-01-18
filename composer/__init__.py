1adbc5ea91434b1ab875b475c49f478d57203fd2
Rexford Essilfie <rexfordessilfie09@gmail.com>
2022-01-18 14:02:25 -0500

    chore: export package members

---
+++ composer/__init__.py
@@ -0,0 +1,10 @@
+from composer.utils import *
+from composer.intervals import *
+from composer.scales import *
+from composer.chords import *
+from composer.pitches import *
+from composer.tone import *
+from composer.songs import *
+
+if __name__ == '__main__':
+    print('Hello Composer :)')
